568f52535daae5749ee17af4d5de0b4ecfaa406d
suyeon woo <woosean999@gmail.com>
2022-04-10 14:29:08 +0900

    copyArray 함수 추출

--- src/cloneDeep.ts
+++ src/cloneDeep.ts
@@ -1,22 +1,22 @@
 import * as utils from './utils/index.js'
 
+function copyArray(value: Array<any>) {
+  return value.reduce((arr, item, i) => {
+    arr[i] = cloneDeep(item)
+    return arr
+  }, [])
+}
+
 function recursive(obj: any) {
   if (utils.isPrimitive(obj) || utils.isProxy(obj) || !utils.hasChild(obj)) {
     return obj
   }
 
-  const cloned = utils.isArray(obj) ? [] : {}
-
   if (utils.isArray(obj)) {
-    obj.length > 0 &&
-      obj.forEach((element: any) => {
-        //@ts-ignore
-        cloned.push(cloneDeep(element))
-      })
-
-    return cloned
+    return copyArray(obj)
   }
 
+  const cloned = {}
   for (const key in obj) {
     //@ts-ignore
     cloned[key] = recursive(obj[key])
